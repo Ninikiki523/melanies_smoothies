51c95a3d84b4a6b9fbbdad7fa4081e4e320a0ec9
Ninikiki523 <167546243+Ninikiki523@users.noreply.github.com>
2024-05-22 13:05:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,10 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections = 5
 )
+fruit_chosen = ingredients_list[0]
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON']
+st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+st.stop()
 
 if ingredients_list:
     #df
